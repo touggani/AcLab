028e657903cb9086425178a0859dad2f00a5a65b
Youssef Touggani <yousseftouggani@MacBook-Pro-de-Youssef.local>
2020-04-08 15:34:58 +0200

    mise en place donnees

---
+++ 1994/ProblemeA/1994PA.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAILLE_MAX = 80
+
+typedef struct{
+	char nom[TAILLE_MAX]
+	char auteur[TAILLE_MAX]
+	int etat;
+}Livre;
+
+
+
