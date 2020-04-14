ef1a5c78e46f7cf5ec7463b32179985883fa1cf3
Havelable <julien.havel@gmail.com>
2020-04-14 13:00:14 +0200

    Ajout Map.addRoute

--- 1995/ProblemeA/main.py
+++ 1995/ProblemeA/main.py
@@ -9,7 +9,13 @@ def main():
         for y in range(len(fileData.weights[x])):
             map.vertices.append(utils.Vertex((x, y), fileData.weights[x][y]))
 
-    
+    for route in fileData.routes:
+        print(route)
+        origin = (route[0], route[1])
+        destination = (route[2], route[3])
+        map.addRoute(origin, destination)
+        print(map.routes)
+
 
 if __name__ == '__main__':
     main()
--- 1995/ProblemeA/modules/utils.py
+++ 1995/ProblemeA/modules/utils.py
@@ -1,3 +1,12 @@
+def getSign(x, y):
+    diff = x - y
+    if diff > 0:
+        return 1
+    elif diff < 0:
+        return -1
+    else:
+         return 0
+
 class Vertex:
     def __init__(self, position, weight):
         self.position = position
@@ -8,3 +17,16 @@ class Map:
     def __init__(self):
         self.vertices = []
         self.routes = []
+
+    def addRoute(self, origin_pos, destination_pos):
+        sign_x = getSign(destination_pos[0], origin_pos[0])
+        if sign_x != 0:
+            print(range(origin_pos[0], destination_pos[0]+sign_x, sign_x))
+            for x in range(origin_pos[0], destination_pos[0]+sign_x, sign_x):
+                self.routes.append((x, origin_pos[1]))
+
+        sign_y = getSign(destination_pos[1], origin_pos[1])
+        if sign_y != 0:
+            print(range(origin_pos[1], destination_pos[1]+sign_y, sign_y))
+            for y in range(origin_pos[1], destination_pos[1]+sign_y, sign_y):
+                self.routes.append((origin_pos[0], y))
