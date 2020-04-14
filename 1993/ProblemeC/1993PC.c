354546877513b06e7e16c550d56a212f3dc53b48
Youssef Touggani <yousseftouggani@MacBook-Pro-de-Youssef.local>
2020-04-14 09:43:10 +0200

    commencement projet c

---
+++ 1993/ProblemeC/1993PC.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+typedef struct noeud {
+	int capaciteAncetre; 
+	int capaciteDescendant; 
+	int nombreDescendant; 
+	int nombreAncetre;
+	int index;
+	int *poids;
+	struct noeud **ancetre
+	struct noeud **desc; 
+	char *nom;
+} NOEUD;
+
+NOEUD *creer_noeud(char *nom){
+	int i;
+	NOEUD *noeud;
+	noeud = malloc(1, sizeof(NOEUD)); noeud->name = malloc(6*sizeof(char)); for(i=0;i<6;i++) noeud->nom[i]=nom[i];
+	NOEUD **descendant;
+	descendant = malloc(5, sizeof(NOEUD)); noeud->descendant=descendant;
+	NOEUD **ancetre;
+	ancetre = malloc(5, sizeof(NOEUD)); noeud->ancetre = ancetre;
+	noeud->nombredDescendant = 0; 
+	noeud->nombreAncetre=0; 
+	noeud->capaciteAncetre=5;
+	noeud->capaciteDescendant=5;
+	noeud->poids=malloc(noeud->capaciteAncetre,sizeof(int)); for(i=0;i<noeud->capaciteAncetre;i++) noeud->poids[i]=0;
+    return noeud;
+}
+
