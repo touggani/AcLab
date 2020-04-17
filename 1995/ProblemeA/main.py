3fddfec0b70ccae2097ca8f6d10c87c72115b4bd
Havelable <julien.havel@gmail.com>
2020-04-17 16:41:31 +0200

    Correction méthode Map.addRoute()

--- 1995/ProblemeA/main.py
+++ 1995/ProblemeA/main.py
@@ -7,12 +7,17 @@ def main():
     map = utils.Map()
     for x in range(len(fileData.weights)):
         for y in range(len(fileData.weights[x])):
-            map.vertices.append(utils.Vertex((x, y), fileData.weights[x][y]))
+            map.addLocation(utils.Location((x+1, y+1), fileData.weights[x][y]))
 
     for route in fileData.routes:
         origin = (route[0], route[1])
         destination = (route[2], route[3])
         map.addRoute(origin, destination)
+
+
+    for location in map.getLocations():
+        print(location.getPosition(), "->", location.getRoutes())
+
 
     print(fileData.routes)
     print(fileData.queries)
--- 1995/ProblemeA/modules/utils.py
+++ 1995/ProblemeA/modules/utils.py
@@ -7,24 +7,53 @@ def getSign(x, y):
     else:
          return 0
 
-class Vertex:
+class Location:
     def __init__(self, position, weight):
-        self.position = position
-        self.weight = weight
+        self.__position = position
+        self.__weight = weight
+        self.__routes = []
+
+
+    def getPosition(self):
+        return self.__position
+
+
+    def getWeight(self):
+        return self.__weight
+
+
+    def addRoute(self, destination_pos):
+        self.__routes.append(destination_pos)
+
+    def getRoutes(self):
+        return self.__routes
 
 
 class Map:
     def __init__(self):
-        self.vertices = []
-        self.routes = []
+        self.__locations = []
+
+
+    def getLocations(self):
+        return self.__locations
+
+    def addLocation(self, location):
+        self.__locations.append(location)
+
+    def getFromPosition(self, position):
+        for location in self.__locations:
+            if location.getPosition() == position:
+                return location
+
+
 
     def addRoute(self, origin_pos, destination_pos):
-        sign_x = getSign(destination_pos[0], origin_pos[0])
-        if sign_x != 0:
-            for x in range(origin_pos[0], destination_pos[0]+sign_x, sign_x):
-                self.routes.append((x, origin_pos[1]))
+            sign_x = getSign(destination_pos[0], origin_pos[0])
+            if sign_x != 0:
+                for x in range(origin_pos[0], destination_pos[0], sign_x):
+                    self.getFromPosition((x, origin_pos[1])).addRoute(self.getFromPosition((x+sign_x, origin_pos[1])))
 
-        sign_y = getSign(destination_pos[1], origin_pos[1])
-        if sign_y != 0:
-            for y in range(origin_pos[1], destination_pos[1]+sign_y, sign_y):
-                self.routes.append((origin_pos[0], y))
+            sign_y = getSign(destination_pos[1], origin_pos[1])
+            if sign_y != 0:
+                for y in range(origin_pos[1], destination_pos[1], sign_y):
+                    self.getFromPosition((origin_pos[0], y)).addRoute(self.getFromPosition((origin_pos[0], y+sign_y)))
